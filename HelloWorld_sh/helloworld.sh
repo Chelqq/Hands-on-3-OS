0fa07494dd5ae83132e4951688bfd51bc586c630
Chel <cdeszz8@gmail.com>
2024-02-27 01:24:59 -0600

    sleep is 4 linux

--- HelloWorld_sh/helloworld.sh
+++ HelloWorld_sh/helloworld.sh
@@ -3,25 +3,26 @@
 # Imprimir "Hello World"
 clear
 echo "Hello World"
-pause 3
+sleep 3
 
 # Listar el contenido del directorio actual
 clear
 echo -e "\nContenido del directorio actual:"
 ls
-pause 3
+sleep 3
 
 # Crear un directorio llamado Test
 clear
 mkdir Test
-pause 3
+sleep 3
 
 # Cambiarse al directorio Test
 clear
 cd Test
-pause 3
+sleep 3
 
 # Listar el contenido del directorio Test
 clear
 echo -e "\nContenido del directorio Test:"
 ls
+
