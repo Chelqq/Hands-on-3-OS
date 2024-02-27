bb24a56bb5bf3b4f669442c1779b0da8319c2451
Chel <cdeszz8@gmail.com>
2024-02-27 01:19:11 -0600

    hello world sh done

---
+++ helloworld.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Imprimir "Hello World"
+clear
+echo "Hello World"
+pause 3
+
+# Listar el contenido del directorio actual
+clear
+echo -e "\nContenido del directorio actual:"
+ls
+pause 3
+
+# Crear un directorio llamado Test
+clear
+mkdir Test
+pause 3
+
+# Cambiarse al directorio Test
+clear
+cd Test
+pause 3
+
+# Listar el contenido del directorio Test
+clear
+echo -e "\nContenido del directorio Test:"
+ls
